5520b4a93add52872baf2a71b7fa5839e118fc24
ZhouFrancisco5g <83173492+Francisco5g@users.noreply.github.com>
2022-03-28 16:40:22 -0300

    Primeiro tema

---
+++ tema1.py
@@ -0,0 +1,10 @@
+from random import randint
+
+numero_aleatorio = randint(0, 10)
+
+numero_digitado = int(input('Escolha um número: '))
+
+if numero_digitado == numero_aleatorio:
+    print('Parabéns, você acertou!!')
+else:
+    print(f'Infelizmene você não acertou! o número era: {numero_digitado}')
